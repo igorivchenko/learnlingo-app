be077e4e892a367b90f1b9acd17528f0099a03a6
Ihor <iv4enkoigor@gmail.com>
2025-10-02 15:03:43 +0300

    fix SettingsPopover

--- src/components/Poppers/SettingsPopover/SettingsPopover.jsx
+++ src/components/Poppers/SettingsPopover/SettingsPopover.jsx
@@ -1,17 +1,12 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import BasePopover from '@/components/Poppers/BasePopover';
 import { settingsPopoverSx } from './SettingsPopover.sx';
 
 const SettingsPopover = ({ onConfirm, onClose, ...props }) => {
   return (
     <BasePopover onClose={onClose} {...props}>
-      <Typography sx={settingsPopoverSx.title}>
-        <b>{'Are you sure?'}</b>
-      </Typography>
       <Box sx={settingsPopoverSx.actionsBox}>
-        <Button sx={settingsPopoverSx.confirmButton} onClick={onConfirm}>
-          Settings
-        </Button>
+        <Button sx={settingsPopoverSx.confirmButton}>Settings</Button>
       </Box>
     </BasePopover>
   );
